03f93c5982c9c08752b79220fde17168ef8f01a5
“procuratura-dev” <speloman638@gmail.com>
2024-02-13 11:48:11 +0200

    123

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$nume = 'Иванов';
+$prenume = 'Николай';
+echo 'Фамилия клиента '.$nume. ', а его имя ' 
+.$prenume . '.';
+$varsta = 30;
+print '<br />Возраст клиента ' .$varsta .'.'
+?>
+
+</body>
+</html>
